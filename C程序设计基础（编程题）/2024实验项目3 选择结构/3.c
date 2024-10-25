f5a20374fe2948e7169b9c0c494a6aec7ffcf319
MinOS-WJ <13871575863@139.com>
2024-10-25 10:23:15 +0800

    Add files via upload

---
+++ C程序设计基础（编程题）/2024实验项目3 选择结构/3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	char c;
+	scanf("%c", &c);
+	if (c >= 'A' && c <= 'Z') {
+		printf("upper\n");
+	} else if (c >= 'a' && c <= 'z') {
+		printf("lower\n");
+	} else if (c >= '0' && c <= '9') {
+		printf("digit\n");
+	} else {
+		printf("other\n");
+	}
+	return 0;
+}
